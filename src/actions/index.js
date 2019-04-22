b52ae7f8e74da2936901923d187333705333e75f
amku91 <amit.kumar@wellthy.care>
2019-04-22 11:20:49 +0530

    Defined type and action

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import Types from '../types';
+import axios from 'axios';
+
+const apiUrl = 'https://api.github.com/users/amku91';
+
+export const fetchData = data => ({
+    type: Types.FETCH_GITHUB_DATA,
+    data
+});
+
+/**get data from github thorugh axios promise */
+export const fetchGithubData = () => {
+    return (dispatch) => {
+        return axios.get(apiUrl).then(response => {
+            dispatch(fetchData(response.data));
+        }).catch(err => {
+            throw new Error(err);
+        })
+    };
+};
---
+++ src/types/index.js
@@ -0,0 +1,5 @@
+const Types = {
+    FETCH_GITHUB_DATA: 'FETCH_GITHUB_DATA'
+};
+
+export default Types;
